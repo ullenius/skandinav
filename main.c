be36bf58a3f006a56a0db6a262341c09a38cb083
Anosh D. Ullenius <anosh@anosh.se>
2024-12-01 20:24:11 +0100

    Refactoring. Remove variable, replace magic number with constant

--- main.c
+++ main.c
@@ -19,9 +19,10 @@
 */
 #include <stdio.h>
 
+#define HI_BYTE 0xC3
+
 int main(int argc, char **argv) {
     unsigned char ch = 0;
-    unsigned char ch2 = 0;
     FILE *in = NULL;
     if (argc != 2) {
         fprintf(stderr, "Usage: skandinav filename\n");
@@ -41,26 +42,26 @@ int main(int argc, char **argv) {
         if (val == 0) {
             putchar(ch);
         }
-        else if (ch == 0xC3) {
-            const unsigned int bytesRead = fread(&ch2, 1, 1, in);
+        else if (ch == HI_BYTE) {
+            const unsigned int bytesRead = fread(&ch, 1, 1, in);
             if (bytesRead == 0) {
                 fprintf(stderr, "Broken UTF-8 file\n");
                 fclose(in);
                 return -1;
             }
-            switch (ch2) {
+            switch (ch) {
                 case 0xA5: /* å */
                 case 0xA4: /* ä */
                 case 0xB6: /* ö */
                 case 0x85: /* Å */
                 case 0x84: /* Ä */
                 case 0x96: /* Ö */
-                case 0x98: /* Ø */
                 case 0x86: /* Æ */
+                case 0x98: /* Ø */
                 case 0xA6: /* æ */
                 case 0xB8: /* ø */
+                putchar(HI_BYTE);
                 putchar(ch);
-                putchar(ch2);
                 break;
             }
         }
